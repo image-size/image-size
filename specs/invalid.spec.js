820c7343a5e84310877b75f39c72b7e033dd7e94
Ross Johnson <ross@mazira.com>
2014-06-08 10:42:50 -0500

    Update asyncFileToBuffer to callback with errors, update lookup() to be purely synchronous

--- specs/invalid.spec.js
+++ specs/invalid.spec.js
@@ -14,12 +14,39 @@ describe('Invalid Images', function () {
 
       var calculate = imageSize.bind(null, file);
 
-      it('should throw', function() {
+      it('should throw when called synchronously', function() {
         expect(calculate).to.throwException(function (e) {
           expect(e).to.be.a(TypeError);
           expect(e.message).to.match(/^invalid \w+$/);
         });
       });
+
+      it('should callback with error when called asynchronously', function(done) {
+        calculate(function (e, size) {
+          expect(e).to.be.a(TypeError);
+          expect(e.message).to.match(/^invalid \w+$/);
+          done();
+        });
+      });
     });
   });
+
+   describe('non-existent file', function() {
+
+      var calculate = imageSize.bind(null, 'fakefile.jpg');
+
+      it('should throw when called synchronously', function() {
+        expect(calculate).to.throwException(function (e) {
+          expect(e).to.be.a(Error);
+          expect(e.message).to.match(/^ENOENT.*$/);
+        });
+      });
+
+      it('should callback with error when called asynchronously', function(done) {
+        calculate(function (e, size) {
+          expect(e.message).to.match(/^ENOENT.*$/);
+          done();
+        });
+      });
+    });
 });

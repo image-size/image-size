52d2ff622027bdd74b59fd180623550e8e65560b
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2017-06-26 13:39:56 +0200

    Issues/82/Support for .ico/.cur (#91)

--- specs/valid.spec.js
+++ specs/valid.spec.js
@@ -20,6 +20,51 @@ var sizes = {
     'width': 123,
     'height': 456
   },
+  'specs/images/valid/cur/sample.cur': {
+    'width': 32, 'height': 32
+  },
+  'specs/images/valid/ico/sample.ico': {
+    'width': 32, 'height': 32
+  },
+  'specs/images/valid/ico/sample-compressed.ico': {
+    'width': 32, 'height': 32
+  },
+  'specs/images/valid/ico/sample-256.ico': {
+    'width': 256, 'height': 256
+  },
+  'specs/images/valid/ico/sample-256-compressed.ico': {
+    'width': 256, 'height': 256
+  },
+  'specs/images/valid/ico/multi-size.ico': {
+    'width': 256,
+    'height': 256,
+    'images': [
+      {'width': 256, 'height': 256},
+      {'width': 128, 'height': 128},
+      {'width': 96, 'height': 96},
+      {'width': 72, 'height': 72},
+      {'width': 64, 'height': 64},
+      {'width': 48, 'height': 48},
+      {'width': 32, 'height': 32},
+      {'width': 24, 'height': 24},
+      {'width': 16, 'height': 16}
+    ]
+  },
+  'specs/images/valid/ico/multi-size-compressed.ico': {
+    'width': 256,
+    'height': 256,
+    'images': [
+      {'width': 256, 'height': 256},
+      {'width': 128, 'height': 128},
+      {'width': 96, 'height': 96},
+      {'width': 72, 'height': 72},
+      {'width': 64, 'height': 64},
+      {'width': 48, 'height': 48},
+      {'width': 32, 'height': 32},
+      {'width': 24, 'height': 24},
+      {'width': 16, 'height': 16}
+    ]
+  },
   'specs/images/valid/jpg/large.jpg': {
     'width': 1600,
     'height': 1200
@@ -37,7 +82,12 @@ var sizes = {
 // Test all valid files
 describe('Valid images', function () {
 
-  var validFiles = glob.sync('specs/images/valid/**/*.*');
+  var validFiles = glob
+    .sync('specs/images/valid/**/*.*')
+    .filter(function (file) {
+      return path.extname(file) !== '.md';
+    });
+
   validFiles.forEach(function (file) {
 
     describe(file, function() {
@@ -68,10 +118,24 @@ describe('Valid images', function () {
         var expected = sizes[file] || sizes.default;
         expect(asyncDimensions.width).to.be(expected.width);
         expect(asyncDimensions.height).to.be(expected.height);
+        if (asyncDimensions.images) {
+          asyncDimensions.images.forEach(function (item, index) {
+            var expectedItem = expected.images[index];
+            expect(item.width).to.be(expectedItem.width);
+            expect(item.height).to.be(expectedItem.height);
+          })
+        }
 
         if (type !== 'tiff') {
           expect(bufferDimensions.width).to.be(expected.width);
           expect(bufferDimensions.height).to.be(expected.height);
+          if (bufferDimensions.images) {
+            bufferDimensions.images.forEach(function (item, index) {
+              var expectedItem = expected.images[index];
+              expect(item.width).to.be(expectedItem.width);
+              expect(item.height).to.be(expectedItem.height);
+            })
+          }
         }
       });
     });

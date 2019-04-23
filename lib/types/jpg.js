image-size/image-size

commit e33bc4ba425463c2c737cba953393f0e0696b415

Support little endian in exif orientation extraction

lib/types/jpg.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var readUInt = require('../readUInt');
+
 // NOTE: we only support baseline and progressive JPGs here
 // due to the structure of the loader class, we only get a buffer
 // with a maximum size of 4096 bytes. so if the SOF marker is outside
@@ -25,7 +27,7 @@ function extractSize (buffer, i) {
 var APP1_DATA_SIZE_BYTES = 2;
 var EXIF_HEADER_BYTES = 6;
 var TIFF_BYTE_ALIGN_BYTES = 2;
-var MOTOROLA_BYTE_ALIGN = '4d4d';
+var BIG_ENDIAN_BYTE_ALIGN = '4d4d';
 
 // Each entry is exactly 12 bytes
 var IDF_ENTRY_BYTES = 12;
@@ -35,11 +37,9 @@ function extractOrientation (buffer, i) {
   // Skip APP1 Data Size
   var exifBlock = buffer.slice(APP1_DATA_SIZE_BYTES, i);
 
-  // TODO: support intel byte alignment
+  // Consider byte alignment
   var byteAlign = exifBlock.toString('hex', EXIF_HEADER_BYTES, EXIF_HEADER_BYTES + TIFF_BYTE_ALIGN_BYTES);
-  if (byteAlign !== MOTOROLA_BYTE_ALIGN) {
-    return;
-  }
+  var bigEndian = byteAlign === BIG_ENDIAN_BYTE_ALIGN;
 
   // TODO: assert that this contains 0x002A
   // var STATIC_MOTOROLA_TIFF_HEADER_BYTES = 2;
@@ -52,7 +52,7 @@ function extractOrientation (buffer, i) {
   // (so the offset includes the tiff byte align)
   var offset = EXIF_HEADER_BYTES + idfOffset;
 
-  var idfDirectoryEntries = exifBlock.readUInt16BE(offset);
+  var idfDirectoryEntries = readUInt(exifBlock, 16, offset, bigEndian);
 
   var start;
   var end;
@@ -61,23 +61,23 @@ function extractOrientation (buffer, i) {
     end = start + IDF_ENTRY_BYTES;
 
     var block = exifBlock.slice(start, end);
-    var tagNumber = block.readUInt16BE(0);
+    var tagNumber = readUInt(block, 16, 0, bigEndian);
 
     // 0x0112 (decimal: 274) is the `orientation` tag ID
     if (tagNumber === 274) {
-      var dataFormat = block.readUInt16BE(2);
+      var dataFormat = readUInt(block, 16, 2, bigEndian);
       if (dataFormat !== 3) {
         return;
       }
 
       // unsinged int has 2 bytes per component
       // if there would more than 4 bytes in total it's a pointer
-      var numberOfComponents = block.readUInt32BE(4);
+      var numberOfComponents = readUInt(block, 32, 4, bigEndian);
       if (numberOfComponents !== 1) {
         return;
       }
 
-      var orientation = block.readUInt16BE(8);
+      var orientation = readUInt(block, 16, 8, bigEndian);
 
       return orientation;
     }

specs/valid.spec.js
@@ -81,7 +81,12 @@ var sizes = {
     'width': 4800,
     'height': 3600
   },
-  'specs/images/valid/jpg/1x2-flipped.jpg': {
+  'specs/images/valid/jpg/1x2-flipped-big-endian.jpg': {
+    'width': 1,
+    'height': 2,
+    'orientation': 8
+  },
+  'specs/images/valid/jpg/1x2-flipped-little-endian.jpg': {
     'width': 1,
     'height': 2,
     'orientation': 8

commit 48d77151a1a594160315fdab8b333d9a76d80d28

jpeg: ignore empty EXIF blocks
fix the regression from #150

lib/types/jpg.js
@@ -28,19 +28,29 @@ var APP1_DATA_SIZE_BYTES = 2;
 var EXIF_HEADER_BYTES = 6;
 var TIFF_BYTE_ALIGN_BYTES = 2;
 var BIG_ENDIAN_BYTE_ALIGN = '4d4d';
+var LITTLE_ENDIAN_BYTE_ALIGN = '4949';
 
 // Each entry is exactly 12 bytes
 var IDF_ENTRY_BYTES = 12;
 var NUM_DIRECTORY_ENTRIES_BYTES = 2;
 
-function extractOrientation (buffer, i) {
+function validateExifBlock (buffer, i) {
   // Skip APP1 Data Size
   var exifBlock = buffer.slice(APP1_DATA_SIZE_BYTES, i);
 
   // Consider byte alignment
   var byteAlign = exifBlock.toString('hex', EXIF_HEADER_BYTES, EXIF_HEADER_BYTES + TIFF_BYTE_ALIGN_BYTES);
-  var bigEndian = byteAlign === BIG_ENDIAN_BYTE_ALIGN;
 
+  // Ignore Empty EXIF. Validate byte alignment
+  var isBigEndian = byteAlign === BIG_ENDIAN_BYTE_ALIGN;
+  var isLittleEndian = byteAlign === LITTLE_ENDIAN_BYTE_ALIGN;
+
+  if (isBigEndian || isLittleEndian) {
+    return extractOrientation(exifBlock, isBigEndian);
+  }
+}
+
+function extractOrientation (exifBlock, isBigEndian) {
   // TODO: assert that this contains 0x002A
   // var STATIC_MOTOROLA_TIFF_HEADER_BYTES = 2;
   // var TIFF_IMAGE_FILE_DIRECTORY_BYTES = 4;
@@ -52,7 +62,7 @@ function extractOrientation (buffer, i) {
   // (so the offset includes the tiff byte align)
   var offset = EXIF_HEADER_BYTES + idfOffset;
 
-  var idfDirectoryEntries = readUInt(exifBlock, 16, offset, bigEndian);
+  var idfDirectoryEntries = readUInt(exifBlock, 16, offset, isBigEndian);
 
   var start;
   var end;
@@ -61,25 +71,23 @@ function extractOrientation (buffer, i) {
     end = start + IDF_ENTRY_BYTES;
 
     var block = exifBlock.slice(start, end);
-    var tagNumber = readUInt(block, 16, 0, bigEndian);
+    var tagNumber = readUInt(block, 16, 0, isBigEndian);
 
     // 0x0112 (decimal: 274) is the `orientation` tag ID
     if (tagNumber === 274) {
-      var dataFormat = readUInt(block, 16, 2, bigEndian);
+      var dataFormat = readUInt(block, 16, 2, isBigEndian);
       if (dataFormat !== 3) {
         return;
       }
 
       // unsinged int has 2 bytes per component
       // if there would more than 4 bytes in total it's a pointer
-      var numberOfComponents = readUInt(block, 32, 4, bigEndian);
+      var numberOfComponents = readUInt(block, 32, 4, isBigEndian);
       if (numberOfComponents !== 1) {
         return;
       }
 
-      var orientation = readUInt(block, 16, 8, bigEndian);
-
-      return orientation;
+      return readUInt(block, 16, 8, isBigEndian);
     }
   }
 }
@@ -106,9 +114,8 @@ function calculate (buffer) {
     // read length of the next block
     i = buffer.readUInt16BE(0);
 
-
     if (isEXIF(buffer)) {
-      orientation = extractOrientation(buffer, i);
+      orientation = validateExifBlock(buffer, i);
     }
 
     // ensure correct format
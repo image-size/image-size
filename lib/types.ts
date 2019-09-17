0192f77a9e167fc81dae5861575f87080f1036bc
Aditya <aditya@netroy.in>
2019-09-17 20:19:45 +0200

    Add support for KTX & PNM formats

--- lib/types.ts
+++ lib/types.ts
@@ -7,7 +7,9 @@ import { GIF } from './types/gif'
 import { ICNS } from './types/icns'
 import { ICO } from './types/ico'
 import { JPG } from './types/jpg'
+import { KTX } from './types/ktx'
 import { PNG } from './types/png'
+import { PNM } from './types/pnm'
 import { PSD } from './types/psd'
 import { SVG } from './types/svg'
 import { TIFF } from './types/tiff'
@@ -21,7 +23,9 @@ export const typeHandlers: {[key: string]: IImage} = {
   icns: ICNS,
   ico: ICO,
   jpg: JPG,
+  ktx: KTX,
   png: PNG,
+  pnm: PNM,
   psd: PSD,
   svg: SVG,
   tiff: TIFF,

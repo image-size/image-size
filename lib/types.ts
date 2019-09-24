2b9d3c347b215930cc8df4cf909b79201518329d
Aditya <aditya@netroy.in>
2019-09-24 17:56:16 +0200

    Added JPEG2000 support
    first cut. needs improvements

--- lib/types.ts
+++ lib/types.ts
@@ -6,6 +6,8 @@ import { DDS } from './types/dds'
 import { GIF } from './types/gif'
 import { ICNS } from './types/icns'
 import { ICO } from './types/ico'
+import { J2C } from './types/j2c'
+import { JP2 } from './types/jp2'
 import { JPG } from './types/jpg'
 import { KTX } from './types/ktx'
 import { PNG } from './types/png'
@@ -22,6 +24,8 @@ export const typeHandlers: {[key: string]: IImage} = {
   gif: GIF,
   icns: ICNS,
   ico: ICO,
+  j2c: J2C,
+  jp2: JP2,
   jpg: JPG,
   ktx: KTX,
   png: PNG,

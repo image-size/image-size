14196ea80753f47fc6d59ab2fc6459f3532d40eb
Aditya <aditya@netroy.in>
2019-12-18 19:45:55 +0100

    debug #260

--- lib/detector.ts
+++ lib/detector.ts
@@ -16,15 +16,24 @@ const firstBytes: { [byte: number]: string } = {
   0xff: 'jpg'
 }
 
+// tslint:disable: no-console
 export function detector(buffer: Buffer): string | undefined {
+  console.log('Initial buffer length', buffer.length)
+
   const byte = buffer[0]
   if (byte in firstBytes) {
     const type = firstBytes[byte]
+    console.log(`Quick lookup for ${type}. Buffer length`, buffer.length)
     if (typeHandlers[type].validate(buffer)) {
       return type
     }
   }
 
-  const finder = (key: string) => typeHandlers[key].validate(buffer)
+  console.log('Quick lookup wasn\'t helpful')
+
+  const finder = (type: string) => {
+    console.log(`Full lookup for ${type}. Buffer length`, buffer.length)
+    return typeHandlers[type].validate(buffer)
+  }
   return keys.find(finder)
 }

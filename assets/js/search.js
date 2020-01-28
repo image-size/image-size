8fd7654a563f86855781a079767428af6147a397
netroy <netroy@users.noreply.github.com>
2020-01-28 23:04:00 +0000

    Automated deployment: Tue Jan 28 23:04:00 UTC 2020 ba5ab576d69676bfd80575c2f052efe3d1be04a6

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","32":"Variable","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"imageSize","url":"modules/_index_.html#imagesize","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""},{"id":2,"kind":64,"name":"setConcurrency","url":"modules/_index_.html#setconcurrency","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""},{"id":3,"kind":32,"name":"types","url":"modules/_index_.html#types","classes":"tsd-kind-variable tsd-parent-kind-external-module","parent":"\"index\""}]};

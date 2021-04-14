c51b9204496da412455d60ce240cd3b425854ecf
Aditya <aditya@netroy.in>
2021-04-14 17:56:25 +0200

    run spell-check across the repo

--- lib/types/j2c.ts
+++ lib/types/j2c.ts
@@ -2,7 +2,7 @@ import { IImage } from './interface'
 
 export const J2C: IImage = {
   validate(buffer) {
-    // TODO: this doesn't seem right. SIZ marker doesnt have to be right after the SOC
+    // TODO: this doesn't seem right. SIZ marker doesn't have to be right after the SOC
     return buffer.toString('hex', 0, 4) === 'ff4fff51'
   },
 

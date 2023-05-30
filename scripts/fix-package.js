bee75b49058950ebc3bdf138c08a4c3cd6db4601
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-05-30 13:35:17 +0200

    Include ESM builds in the released package

---
+++ scripts/fix-package.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path')
+const { writeFileSync, renameSync } = require('fs')
+const { sync: globSync } = require('glob')
+
+const distDir = resolve(__dirname, '../dist')
+
+writeFileSync(
+  resolve(distDir, 'cjs/package.json'),
+  '{ "type": "commonjs" }\n',
+  'utf-8'
+)
+
+writeFileSync(
+  resolve(distDir, 'esm/package.json'),
+  '{ "type": "module" }\n',
+  'utf-8'
+)
+
+const esmDir = resolve(distDir, 'esm')
+const esmFiles = globSync('**/*.js', { cwd: esmDir })
+for (const file of esmFiles) {
+  renameSync(
+    resolve(esmDir, file),
+    resolve(esmDir, file.replace(/\.js$/, '.mjs'))
+  )
+}

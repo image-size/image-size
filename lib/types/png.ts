ef22391af058163a1483df8069421b84ef83ff7a
Victor Widell <victor.widell@gmail.com>
2025-02-18 12:59:04 +0100

    Fixed url. The domain had expired.

--- lib/types/png.ts
+++ lib/types/png.ts
@@ -4,7 +4,7 @@ import { readUInt32BE, toUTF8String } from './utils'
 const pngSignature = 'PNG\r\n\x1a\n'
 const pngImageHeaderChunkName = 'IHDR'
 
-// Used to detect "fried" png's: http://www.jongware.com/pngdefry.html
+// Used to detect "fried" png's: https://web.archive.org/web/20190414220044/http://www.jongware.com/pngdefry.html
 const pngFriedChunkName = 'CgBI'
 
 export const PNG: IImage = {
